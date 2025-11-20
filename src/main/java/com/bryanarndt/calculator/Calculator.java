cf3a7fb743b17e5f0e72100a5456204f56ef0d64
Bryan Arndt <bryan.arndt52@gmail.com>
2025-11-19 17:13:03 -0800

    Initial commit

---
+++ src/main/java/com/bryanarndt/calculator/Calculator.java
@@ -0,0 +1,41 @@
+package com.bryanarndt.calculator;
+
+import java.util.Scanner;
+
+
+public class Calculator {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        boolean continueCalculating = true;
+
+        while(continueCalculating){
+            System.out.println("Enter the first number.");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Enter the second number.");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Enter an operator " + "(+, -, *, /)");
+            String operatorInput = scanner.next();
+
+            if(operatorInput.equals("+")){
+                double userAnswer = num1 + num2;
+                System.out.println("Result: " + userAnswer);
+            }else if(operatorInput.equals("-")){
+                double userAnswer = num1 - num2;
+                System.out.println("Result: " + userAnswer);
+            }else if(operatorInput.equals("*")){
+                double userAnswer = num1 * num2;
+                System.out.println("Result: " + userAnswer);
+            }else if(operatorInput.equals("/")){
+                double userAnswer = num1 / num2;
+                System.out.println("Result: " + userAnswer);
+            }else{
+                System.out.println("Invalid operator");
+            }
+
+        }
+    }
+}
